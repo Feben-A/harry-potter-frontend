eef4b2ecde59b84487f1b5bbea97dda1e804cdef
Feben-A <afa508165@gmail.com>
2024-08-20 23:21:14 +0100

    fix bug card flip functionality wasn't working

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
-const name = document.querySelector(".name");
-const main = document.querySelector("main");
+const main = document.getElementById("main");
+const character = document.querySelector(".character");
 const searchInput = document.getElementById("search");
 
 let characterArray = [];
+
 searchInput.addEventListener("input", (e) => {
   const value = e.target.value.toLowerCase();
   console.log(value);
@@ -15,9 +16,10 @@ searchInput.addEventListener("input", (e) => {
 getCharacters();
 
 function getCharacters() {
-  fetch("https://potterhead-api.vercel.app/api/characters")
+  fetch("https://harrypotter-backend-api-workshop.onrender.com/characters")
     .then((res) => res.json())
     .then((data) => {
+      console.log(data);
       showCharacters(data);
     });
 }
@@ -27,25 +29,37 @@ function showCharacters(data) {
 
   characterArray = data.map((character) => {
     const { name, image } = character;
-    const characterEl = document.createElement("div");
-    characterEl.classList.add("character", "front");
 
-    characterEl.innerHTML = `<img src= "${image}" alt="${name}" onerror="handleError(this)";/>
-    <div class='character-info'>
-    <h3>${name}</h3></div>`;
+    const characterEl = document.createElement("div");
+    characterEl.classList.add("card");
+
+    const cardInner = document.createElement("div");
+    cardInner.classList.add("card-inner");
+
+    const characterFront = document.createElement("div");
+    characterFront.classList.add("front");
+    characterFront.innerHTML = `
+     <img src="${image}" alt="${name}" onerror="handleError(this);"/>
+     <div class="character-info">
+     <h3>${name}</h3>
+     </div>
+     `;
 
     const characterBack = document.createElement("div");
-    characterBack.classList.add("character", "back");
-    main.appendChild(characterEl);
-    characterBack.innerHTML = "<h1>Back of the card</h1>";
+    characterBack.classList.add("back");
+    characterBack.innerHTML = `
+     <h1>Back of the card</h1>`;
+
+    cardInner.appendChild(characterFront);
+    cardInner.appendChild(characterBack);
+
+    characterEl.appendChild(cardInner);
 
     main.appendChild(characterEl);
-    main.appendChild(characterBack);
-
     return { name: character.name, element: characterEl };
   });
 }
 
 function handleError(imgElement) {
-  imgElement.src = "src/wizard-icon.jpeg";
+  imgElement.src = "images/wizard-icon.jpeg";
 }
